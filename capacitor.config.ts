0a182cec98728746f3d8c093e1fe19d79f14c9f7
AngelF4 <angelfhernandez71@gmail.com>
2023-10-02 19:27:55 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppMoviles1-2011704',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
